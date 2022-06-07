ff27a8a2c8c29f882d2c4728e0932f8b08d5f0aa
El7 <enejecaleb@gmail.com>
2022-06-07 14:12:56 +0000

    project completed

--- text-analyzer/js/scripts.js
+++ text-analyzer/js/scripts.js
@@ -1,29 +1,51 @@
-const textareaEl = $("#text").val();
-let text = 0
+const textareaEl = document.querySelector("#text");
+let text = null;
 
-const data = () => {
-  words = 0,
-  sentence = 0,
-  upperCase = 0,
-  lowerCase = 0,
-  spaces = 0,
-  digits = 0,
-  vowels = 0,
-  consonants = 0
+let data = {
+  words: 0,
+  sentence: 0,
+  upperCase: 0,
+  lowerCase: 0,
+  spaces: 0,
+  digits: 0,
+  vowels: 0,
+  consonants: 0,
 };
+
+const findlength = (item) => (item === null ? 0 : item.length);
 
 const setText = () => {
   text = textareaEl.value;
 
-  if(text != 0){
-    data.sentence = text.match(/\056/g);
-    data.words = text.match(/a-z/gi);
-    data.spaces = text.match(/\s/g);
-    data.upperCase = text.match(/[A-Z]/g);
-    data.lowerCase = text.match(/[a-z]/);
-    data.digits = text.match(/\d/g);
-    data.vowels = text.match(/[aeiou]/gi);
-    data.consonants = text.match(/[bcdfghjklmnpqrstvwxyz]/gi);
-    
+  if (text != null) {
+    data.sentence = findlength(text.match(/\056/g));
+    data.words = findlength(text.match(/[a-zA-Z]+/g));
+    data.spaces = findlength(text.match(/\s/g));
+    data.upperCase = findlength(text.match(/[A-Z]/g));
+    data.lowerCase = findlength(text.match(/[a-z]/g));
+    data.digits = findlength(text.match(/\d/g));
+    data.vowels = findlength(text.match(/[aeiou]/gi));
+    data.consonants = findlength(text.match(/[bcdfghjklmnpqrstvwxyz]/gi));
   }
+
+  localStorage.setItem("data", JSON.stringify(data));
+
+  window.location = "info.html";
+};
+
+const getData = () => {
+  return JSON.parse(localStorage.getItem("data"));
+};
+
+const showData = () => {
+  let data = getData();
+  let htmlContent = ""
+
+  for (item in data) {
+    htmlContent += `<div class ='box'>
+    <h2>${data[item]}</h2>
+    <p>${item}</p>
+    </div>`
+  }
+  document.querySelector(".info-wrapper").innerHTML = htmlContent;
 }
